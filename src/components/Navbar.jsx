bf9150f3234bff2e91838536e353d87aa1943ff8
Ana Ea <aabad0464@gmail.com>
2021-11-08 17:52:48 -0700

    setting navbar icon & fixing favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,21 @@ function Navbar() {
   <>
   <nav className="navbar">
       <div className="navbarContainer">
+          {/* LogoRef */}
           <Link to='/' className='navbarLogo'>
-              Logo Here
+          <img
+                  src={"images/moon.png"}
+                 alt="moon icon"
+                  />Home
           </Link>
+          {/* Hamburger Menu */} 
+          <div className="menuIcon">
+              <icon className=""/>
+          </div>
       </div>
   </nav>
   </>)
 }
 
 export default Navbar;
+
