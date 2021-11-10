150a77d3475272c380f7cd5b8bb47dfb17907bc8
Ana Ea <aabad0464@gmail.com>
2021-11-10 13:34:37 -0700

    importing components to projectlist

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ProjectList.css';
+import ProjectList from './Project';
+
+function ProjectList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProjectList
