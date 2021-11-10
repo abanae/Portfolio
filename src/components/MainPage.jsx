a2954109752e04ed1be53e1ebc7ea274b415e00e
Ana Ea <aabad0464@gmail.com>
2021-11-09 21:44:09 -0700

    setting layout for MainPage & video

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+
 function MainPage() {
     return (
         <div className="mainContainer">
         {/* Video Setting */}
-            
+            <video src='/video/camping.mp4' autoPlay loop muted/>
+            <h1>Me!!</h1>
+            <p>What I Do</p>
+            <div className="mainBtn">
+            </div>
         </div>
     )
 }
