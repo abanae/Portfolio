bed41cbf9c0334f1b6318b86a46c4ef1b0832b69
Ana Ea <aabad0464@gmail.com>
2021-11-10 00:54:39 -0700

    adding style into Button on main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -8,13 +8,11 @@ function MainPage() {
         <div className="mainContainer">
         {/* Video Setting */}
             <video src='/video/camping.mp4' autoPlay loop muted/>
-            <h1>Me!!</h1>
-            <p>What I Do</p>
+            <h1>Hello,</h1>
+            <p>I'm a full-stack web developer,  focused on crafting clean & user‑friendly experiences.</p>
             <div className="mainBtn">
                 <Button className="btns" btnStyle='btn--outline'
-                btnSize='btn--large'>Check Me Out</Button>
-                <Button className="btns" btnStyle='btn--primary'
-                btnSize='btn--large'>Lookie</Button>
+                btnSize='btn--large'>Check My Projects</Button>
             </div>
         </div>
     )
