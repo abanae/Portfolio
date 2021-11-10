f7a1735e299d9907fcd15f42f4ff8711b7ebf9a0
Ana Ea <aabad0464@gmail.com>
2021-11-09 21:36:58 -0700

    creating mainpage component

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainPage() {
+    return (
+        <div className="mainContainer">
+        {/* Video Setting */}
+            
+        </div>
+    )
+}
+
+export default MainPage
