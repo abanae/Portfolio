aa8af942c20319095975d5a2000963c41f588fff
Ana Ea <aabad0464@gmail.com>
2021-11-15 15:53:12 -0700

    installing material Ui & components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
 
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from '@mui/material/Button';
 
-function Project(props) {
-    return (
-        <>
-            <li className='projectItem'>
-        <Link className='projectItemLink' to={props.path}>
-          <figure className='projectItemPicWrap' data-category={props.label}>
-            <img
-              className='projectItemImg'
-              alt='Projects'
-              src={props.src}
-            />
-          </figure>
-          <div className='projectItemInfo'>
-            <h5 className='projectItemText'>{props.text}</h5>
-          </div>
-        </Link>
-      </li>
-        </>
-    )
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard(props) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        title={props.label}
+        subheader={props.date}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+       image={props.src}
+        alt="Really Good Fake Store"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        {props.text}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+            set aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+            stirring occasionally until lightly browned, 6 to 8 minutes.
+          </Typography>
+          <CardActions>
+        <Button size="small">Github</Button>
+        <Button size="small">Deployed Site</Button>
+      </CardActions>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
-
-export default Project
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ProjectList.css';
-import Project from './Project';
+import RecipeReviewCard from './Project';
 
 function ProjectList() {
     return (
@@ -9,41 +9,44 @@ function ProjectList() {
             <div className='projectsContainer'>
      <div className='projectsWrapper'>
           <ul className='projectsItem'>
-            <Project
+            {/* <Project
               src='images/SocialPetwork.PNG'
               text="Deepen your lyrical knowledge by using our dictionary functions to look up any 'slang' term from your favorite songs.Or further your 'traditional' knowledge with our Webster dictionary search."
-              label='The Social Petwork'
-              path='/projects'
-            />
-                <Project
+              label='LIVE The Social Petwork'
+             
+            /> */}
+                <RecipeReviewCard
+              date = 'August,2021'
               src='images/fakeStore.PNG'
               text="Our project is a mock e-commerce store using seeded data to simulate real e-commerce website. Users can
               login/logout & add/delete products from their shopping cart.
               "
               label='Really Good Fake Store'
-              path='/projects'
+              buttonDeploy= 'https://really-good-fake-store.herokuapp.com/'
+              labelGithub='Github'
+              buttonUrl= 'https://github.com/abanae/Really-Good-Fake-Store'
             />
           </ul>
-          <ul className='projectsItem'>
+          {/* <ul className='projectsItem'>
           <Project
               src='images/urbanCi.PNG'
               text="Deepen your lyrical knowledge by using our dictionary functions to look up any 'slang' term from your favorite songs.Or further your 'traditional' knowledge with our Webster dictionary search."
               label='Urban Cipher'
-              path='/projects'
+             
             />
             <Project
               src='images/GuessMN.PNG'
               text='A simple guessing game where a user is supposed to guess a number between 0 and 20 in a maximum of 20 attempts. The game will end after 20 attempts, game also keeps track of the highesst scored'
               label='Guess My Number'
-              path='/projects'
+              
             />
             <Project
               src='images/Tech.PNG'
               text='A blog made for developers to publish their ideas/projects as  posts and comment on other developers’ posts as well. Developers must create an account to view, create, update, & delete blogs.'
               label='Tech Blog'
-              path='/projects'
+              
             />
-          </ul>
+          </ul> */}
         </div>
       </div>   
         </div>
