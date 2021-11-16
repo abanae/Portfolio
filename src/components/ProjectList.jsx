4192dc5452110919bb009a098a2dfce768d980f7
Ana Ea <aabad0464@gmail.com>
2021-11-15 17:00:37 -0700

    structure key values in project list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -9,44 +9,58 @@ function ProjectList() {
             <div className='projectsContainer'>
      <div className='projectsWrapper'>
           <ul className='projectsItem'>
-            {/* <Project
+            <RecipeReviewCard
+              label='The Social Petwork'
+              date = 'October,2021'
               src='images/SocialPetwork.PNG'
-              text="Deepen your lyrical knowledge by using our dictionary functions to look up any 'slang' term from your favorite songs.Or further your 'traditional' knowledge with our Webster dictionary search."
-              label='LIVE The Social Petwork'
+              alt="The Social Petwork Cover"
+              text="This project is a social media application for pets! The user can signup/login, make post, like posts and view the timeline. The user can also edit or delete their own posts"
+              buttonDeploy= 'https://serene-tundra-79280.herokuapp.com/registerpage'
+              buttonUrl= 'https://github.com/abanae/The-Social-Petwork'
+              
              
-            /> */}
+            />
                 <RecipeReviewCard
+              label='Really Good Fake Store'
               date = 'August,2021'
               src='images/fakeStore.PNG'
-              alt="Really Good Fake Store"
+              alt="Really Good Fake Store Cover"
               text="Our project is a mock e-commerce store using seeded data to simulate real e-commerce website. Users can
               login/logout & add/delete products from their shopping cart.
               "
-              label='Really Good Fake Store'
               buttonDeploy= 'https://really-good-fake-store.herokuapp.com/'
               buttonUrl= 'https://github.com/abanae/Really-Good-Fake-Store'
             />
           </ul>
-          {/* <ul className='projectsItem'>
-          <Project
-              src='images/urbanCi.PNG'
-              text="Deepen your lyrical knowledge by using our dictionary functions to look up any 'slang' term from your favorite songs.Or further your 'traditional' knowledge with our Webster dictionary search."
-              label='Urban Cipher'
-             
-            />
-            <Project
-              src='images/GuessMN.PNG'
-              text='A simple guessing game where a user is supposed to guess a number between 0 and 20 in a maximum of 20 attempts. The game will end after 20 attempts, game also keeps track of the highesst scored'
+          <ul className='projectsItem'>
+            <RecipeReviewCard
               label='Guess My Number'
-              
+              date = 'November,2021'
+              src='images/GuessMN.PNG'
+              alt="Guess My Number Cover"
+              text='A simple guessing game where a user is supposed to guess a number between 0 and 20 in a maximum of 20 attempts. The game will end after 20 attempts, game also keeps track of the highesst scored'
+              buttonDeploy= 'https://abanae.github.io/Guess-My-Number-Game/'
+              buttonUrl= 'https://github.com/abanae/Guess-My-Number-Game' 
             />
-            <Project
+            <RecipeReviewCard
+              label='Urban Cipher'
+              date = 'June,2021'
+              src='images/urbanCi.PNG'
+              alt="Urban Cipher Cover"
+              text="Deepen your lyrical knowledge by using our dictionary functions to look up any 'slang' term from your favorite songs.Or further your 'traditional' knowledge with our Webster dictionary search."
+              buttonDeploy= 'https://maximosandoval.github.io/urban-cipher/'
+              buttonUrl= 'https://github.com/abanae/urban-cipher'
+            />
+            <RecipeReviewCard
+              label='Tech Blog' 
+              date = 'August,2021'
               src='images/Tech.PNG'
+              alt="Tech Blog over"
               text='A blog made for developers to publish their ideas/projects as  posts and comment on other developers’ posts as well. Developers must create an account to view, create, update, & delete blogs.'
-              label='Tech Blog'
-              
+              buttonDeploy= 'https://awesome-blogger.herokuapp.com/'
+              buttonUrl= 'https://github.com/abanae/Tech_Blog'
             />
-          </ul> */}
+          </ul>
         </div>
       </div>   
         </div>
