4f0d0b7f994e7f48dcb287f6133b3022cb2ed726
Ana Ea <aabad0464@gmail.com>
2022-01-19 23:43:29 -0700

    addidng textfields for contact form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import "./ContactMe.css";
+import  Card  from '@mui/material/Card';
+import  CardContent  from '@mui/material/CardContent';
+import  Grid  from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 
 function ContactMe() {
   return (
-    <div className="">
-    Contact Me
+    <div className="Contact Me">
+  <Card>
+      <CardContent>
+          <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                  <TextField label="First Name" placeholder="Enter First Name" variant="outlined" fullWidth required/>
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                  <TextField label="Last Name" placeholder="Enter Last Name" variant="outlined" fullWidth required/>
+              </Grid>
+              <Grid xs={12} item>
+                  <TextField type="email" label="Email" placeholder="Enter Email" variant="outlined" fullWidth required/>
+              </Grid>
+          </Grid>
+      </CardContent>
+  </Card>
     </div>
   );
 }
