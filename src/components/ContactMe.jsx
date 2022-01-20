c564170325434d817d966a95e113025179c0fad0
Ana Ea <aabad0464@gmail.com>
2022-01-20 00:41:26 -0700

    styling contact form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,41 +1,78 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
 import "./ContactMe.css";
-import  Card  from '@mui/material/Card';
-import  CardContent  from '@mui/material/CardContent';
-import  Grid  from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-
 
 function ContactMe() {
   return (
-    <div className="Contact Me">
-  <Card>
-      <CardContent>
-          <Grid container spacing={1}>
-              <Grid xs={12} sm={6} item>
-                  <TextField label="First Name" placeholder="Enter First Name" variant="outlined" fullWidth required/>
-              </Grid>
-              <Grid xs={12} sm={6} item>
-                  <TextField label="Last Name" placeholder="Enter Last Name" variant="outlined" fullWidth required/>
-              </Grid>
-              <Grid xs={12} item>
-                  <TextField type="email" label="Email" placeholder="Enter Email" variant="outlined" fullWidth required/>
-              </Grid>
-              <Grid xs={12} item>
-                  <TextField type="number" label="Phone Number" placeholder="Enter Phone Number" variant="outlined" fullWidth required/>
-              </Grid>
-              <Grid xs={12} item>
-                  <TextField multiline rows={5} label="Message" placeholder="Type Your Message" variant="outlined" fullWidth required/>
-              </Grid>
-              <Grid xs={12} item>
-                  <Button type="submit" variant="contained" fullWidth >Submit</Button>
-              </Grid>
+      <Card style= {{maxWidth:650, padding:"4vw 1.5vw" }}>
+          <CardContent>
+    <Box component="form">
+      <div>
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} item>
+            <TextField
+              label="First Name"
+              placeholder="Enter First Name"
+              variant="outlined"
+              fullWidth
+              required
+            />
           </Grid>
-      </CardContent>
-  </Card>
-    </div>
+          <Grid xs={12} sm={6} item>
+            <TextField
+              label="Last Name"
+              placeholder="Enter Last Name"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid xs={12} item>
+            <TextField
+              type="email"
+              label="Email"
+              placeholder="Enter Email"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid xs={12} item>
+            <TextField
+              type="number"
+              label="Phone Number"
+              placeholder="Enter Phone Number"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid xs={12} item>
+            <TextField
+              multiline
+              rows={5}
+              label="Message"
+              placeholder="Type Your Message"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid xs={12} item>
+            <Button type="submit" variant="contained" fullWidth>
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    </Box>
+    </CardContent>
+    </Card>
   );
 }
-
-export default ContactMe;
+ export default ContactMe;
