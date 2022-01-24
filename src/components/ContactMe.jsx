c22d59fc9463a418c30181ca8f143e9c6b843ef9
Ana Ea <aabad0464@gmail.com>
2022-01-23 22:34:09 -0700

    testing react google  map

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -78,8 +79,22 @@ function ContactMe() {
       </div>
     </Box>
     </CardContent>
+     {/* <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map> */}
     </Card>
     </div>
   );
 }
  export default ContactMe;
+ //  export default GoogleApiWrapper({
+//     apiKey: ("ENTER KEY")
+//   })(ContactMe)
